0719a341aab79d7195b6dbeddb92a288e50718d8
kapindra <kapindra.raut@gmail.com>
2023-04-10 14:13:10 +0545

    fix: image dimension

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,13 +1,16 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 const Card = ({ slug, date, title, description }: any) => {
   return (
     <Link href={`posts/` + slug}>
       <article className="overflow-hidden rounded-lg shadow transition hover:shadow-lg">
-        <img
+        <Image
           alt="Office"
           src="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
           className="h-56 w-full object-cover"
+          width={300}
+          height={300}
         />
 
         <div className="bg-white p-4 sm:p-6">
