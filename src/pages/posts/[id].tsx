291caf44651b3a09d16b59cc4d914c02880ccec6
kapindra <kapindra.raut@gmail.com>
2023-04-10 08:44:04 +0545

    build: add linting and project setup dependencies

---
+++ src/pages/posts/[id].tsx
@@ -0,0 +1,38 @@
+import Head from "next/head";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+
+const NewPost = ({ postData }: any) => {
+  return (
+    <>
+      <Head>
+        <meta
+          name="description"
+          content="Author: A.N. Author, Illustrator: P. Picture, Category: Books, Price:  £9.24, Length: 784 pages"
+        />
+      </Head>
+      <div>{postData.title}</div>
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </>
+  );
+};
+
+export default NewPost;
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }: any) {
+  // Add the "await" keyword like this:
+  const postData = await getPostData(params.id);
+
+  return {
+    props: {
+      postData,
+    },
+  };
+}
